2294702c69764fc9d297e5c94ccb02f84175230b
Ahmad <anwar98.kn@gmail.com>
2024-08-27 14:52:23 +0300

    fix

--- src/pages/srdHouse/Room.jsx
+++ src/pages/srdHouse/Room.jsx
@@ -359,7 +359,7 @@ const Room = () => {
                       <MicOffOutlined sx={{ fontSize: "18px" }} />
                     </Box>
                   )}
-                  {client.role === "speaker" && isAdmin && (
+                  {(client.role === "speaker" || isAdmin) && (
                     <IconButton
                       id="fade-button"
                       aria-controls={openAud ? "fade-menu-aud" : undefined}
@@ -375,56 +375,70 @@ const Room = () => {
                       <MoreVert sx={{ color: "#f25f0c", cursor: "pointer" }} />
                     </IconButton>
                   )}
-                  <Menu
-                    id="fade-menu-aud"
-                    MenuListProps={{
-                      "aria-labelledby": "fade-button",
-                    }}
-                    anchorEl={anchorElAud?.anchor || null}
-                    open={openAud}
-                    onClose={handleCloseAud}
-                    TransitionComponent={Fade}
-                    sx={{ ml: "-8px", mt: "5px" }}
-                  >
-                    {isAdmin && anchorElAud && (
-                      <>
-                        <MenuItem
-                          sx={{ color: "red" }}
-                          onClick={() => {
-                            blockUser(anchorElAud.clientId);
-                            handleCloseAud();
-                          }}
-                        >
-                          Block the user
-                        </MenuItem>
+                  {isAdmin && anchorElAud && (
+                    <Menu
+                      id="fade-menu-aud"
+                      MenuListProps={{
+                        "aria-labelledby": "fade-button",
+                      }}
+                      anchorEl={anchorElAud?.anchor || null}
+                      open={openAud}
+                      onClose={handleCloseAud}
+                      TransitionComponent={Fade}
+                      sx={{ ml: "-8px", mt: "5px" }}
+                    >
+                      <MenuItem
+                        sx={{ color: "red" }}
+                        onClick={() => {
+                          blockUser(anchorElAud.clientId);
+                          handleCloseAud();
+                        }}
+                      >
+                        Block the user
+                      </MenuItem>
 
-                        {/* Admin can return user to audience */}
-                        <MenuItem
-                          onClick={() => {
-                            returnAudienceSpeak(anchorElAud.clientId);
-                            handleCloseAud();
-                          }}
-                        >
-                          Return to audience
-                        </MenuItem>
-                      </>
+                      {/* Admin can return user to audience */}
+                      <MenuItem
+                        onClick={() => {
+                          returnAudienceSpeak(anchorElAud.clientId);
+                          handleCloseAud();
+                        }}
+                      >
+                        Return to audience
+                      </MenuItem>
+                    </Menu>
+                  )}
+                  {isSpeaker &&
+                    !isAdmin &&
+                    client.role === "speaker" &&
+                    anchorElAud && (
+                      <Menu
+                        id="fade-menu-aud"
+                        MenuListProps={{
+                          "aria-labelledby": "fade-button",
+                        }}
+                        anchorEl={anchorElAud?.anchor || null}
+                        open={openAud}
+                        onClose={handleCloseAud}
+                        TransitionComponent={Fade}
+                        sx={{ ml: "-8px", mt: "5px" }}
+                      >
+                        {/* Speaker can only return user to audience */}
+                        {isSpeaker &&
+                          !isAdmin &&
+                          client.role === "speaker" &&
+                          anchorElAud && (
+                            <MenuItem
+                              onClick={() => {
+                                returnAudienceSpeak(anchorElAud.clientId);
+                                handleCloseAud();
+                              }}
+                            >
+                              Return to audience
+                            </MenuItem>
+                          )}
+                      </Menu>
                     )}
-
-                    {/* Speaker can only return user to audience */}
-                    {isSpeaker &&
-                      !isAdmin &&
-                      client.role === "speaker" &&
-                      anchorElAud && (
-                        <MenuItem
-                          onClick={() => {
-                            returnAudienceSpeak(anchorElAud.clientId);
-                            handleCloseAud();
-                          }}
-                        >
-                          Return to audience
-                        </MenuItem>
-                      )}
-                  </Menu>
                 </Box>
                 <Typography
                   sx={{
